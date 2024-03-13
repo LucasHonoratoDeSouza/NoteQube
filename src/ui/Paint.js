59f62422b01bed9304dc1681e3058688e6f99fa5
LucasHonoratoDeSouza <159482550+LucasHonoratoDeSouza@users.noreply.github.com>
2024-03-12 21:13:31 -0300

    Update Paint.js

--- src/ui/Paint.js
+++ src/ui/Paint.js
@@ -1,72 +1,74 @@
-// var canvas = document.getElementById('canvas')
-// const contentPaint = document.querySelector('.contentPaint')
-// canvas.width = window.innerWidth / 1.25
-// canvas.height = window.innerHeight / 1.25
+/*var canvas = document.getElementById('canvas')
+const contentPaint = document.querySelector('.contentPaint')
+canvas.width = window.innerWidth / 1.25
+canvas.height = window.innerHeight / 1.25
 
-// var bound = canvas.getBoundingClientRect()
-// console.log(bound)
+var bound = canvas.getBoundingClientRect()
+console.log(bound)
 
-// c = canvas.getContext('2d')
+c = canvas.getContext('2d')
 
-// var xlim = canvas.width
-// var ylim = canvas.height
+var xlim = canvas.width
+var ylim = canvas.height
 
-// var color = document.getElementById("color")
-// var radius = document.getElementById("radius")
+var color = document.getElementById("color")
+var radius = document.getElementById("radius")
 
-// var mouse = {
-//     x: 0,
-//     y: 0,
-//     down: false
-// }
+var mouse = {
+    x: 0,
+    y: 0,
+    down: false
+}
 
-// var stroke = []
+var stroke = []
 
-// function draw() {
-//     c.beginPath()
-//     c.fillStyle = color.value
-//     c.arc(mouse.x, mouse.y, Math.abs(Number(radius.value)), 0, Math.PI*2, false)
-//     c.fill()
-//     c.closePath()
-// }
+function draw() {
+    c.beginPath()
+    c.fillStyle = color.value
+    c.arc(mouse.x, mouse.y, Math.abs(Number(radius.value)), 0, Math.PI*2, false)
+    c.fill()
+    c.closePath()
+}
 
-// function erase() {
-//     color.value = "#e9e9e9"
-// }
+function erase() {
+    color.value = "#e9e9e9"
+}
 
-// function eraseWhole() {
-//     c.clearRect(0,0,xlim,ylim)
-// }
+function eraseWhole() {
+    c.clearRect(0,0,xlim,ylim)
+}
 
-// addEventListener("mousemove", (e) => {
-//     mouse.x = e.clientX - bound.left
-//     mouse.y = e.clientY - bound.top
-//     if (mouse.down) {
-//         stroke.push({
-//             x: mouse.x,
-//             y: mouse.y
-//         })
-//         draw()
-//     }
-// })
+addEventListener("mousemove", (e) => {
+    mouse.x = e.clientX - bound.left
+    mouse.y = e.clientY - bound.top
+    if (mouse.down) {
+        stroke.push({
+            x: mouse.x,
+            y: mouse.y
+        })
+        draw()
+    }
+})
 
-// addEventListener("mousedown", (e) => {
-//     mouse.down = true
-// })
+addEventListener("mousedown", (e) => {
+    mouse.down = true
+})
 
-// addEventListener("mouseup", (e) => {
-//     mouse.down = false
-//     for(var i = 0; i<stroke.length-1; i++){
-//         c.beginPath();
-//         c.strokeStyle = color.value;
-//         c.lineWidth = Number(radius.value)*2;
-//         c.moveTo(stroke[i].x, stroke[i].y);
-//         c.lineTo(stroke[i+1].x, stroke[i+1].y);
-//         c.stroke();
-//         c.closePath();
-//     }
-//     stroke = []
+addEventListener("mouseup", (e) => {
+    mouse.down = false
+    for(var i = 0; i<stroke.length-1; i++){
+        c.beginPath();
+        c.strokeStyle = color.value;
+        c.lineWidth = Number(radius.value)*2;
+        c.moveTo(stroke[i].x, stroke[i].y);
+        c.lineTo(stroke[i+1].x, stroke[i+1].y);
+        c.stroke();
+        c.closePath();
+    }
+    stroke = []
 
-// })
+})*/
+
+//função de desenhar removida
 
 
